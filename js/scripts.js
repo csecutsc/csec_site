5d3d6b797a4ea9b39743892907a02f22fce80e74
Kevin Shen <kevin_shen@hotmail.ca>
2017-09-12 02:25:36 -0400

    Linked CSEC division sites to the main site and created responsive skeleton

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $('#logo').fadeIn(2000);
+    $('#title').fadeIn(6000);
+    $('.toggle-nav').click(function (e) {
+        $('#menuitems').slideToggle(400);
+        //$('#main').toggleClass("blur .disable");
+        $(this).toggleClass('active');
+        $('.menu ul').toggleClass('active');
+        e.preventDefault();
+    });
+    // smooth scrolling
+    $("a[href^='#']").on('click', function(e) {
+        var hash = this.hash;
+       $('html, body').animate({
+           scrollTop: $(hash).offset().top
+         }, 1000, function(){
+           window.location.hash = hash;
+         });
+        e.preventDefault();
+    });
+});
+
