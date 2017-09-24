cdf8c76337201161085418d3f6de0f343122faff
ViperXYZ <sammy.haque@mail.utoronto.ca>
2017-09-24 06:13:38 -0400

    reformatted index and fixed syntax error in scipt.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,53 @@
-$(document).ready(function () {
+$(document).ready(function() {
     // smooth scrolling
-    $("a[href^='#']").on('click', function (e) {
+    $("a[href^='#']").on('click', function(e) {
         var hash = this.hash;
         $('html, body').animate({
             scrollTop: $(hash).offset().top - 50
-        }, 1000, function () {
+        }, 1000, function() {
             window.location.hash = hash;
         });
         e.preventDefault();
     });
     // makeshift anti-spam function
-    $('.email-link').hover(function(){
-	var newHref = $(this).attr('href').replace('unhackable', 'mail.utoronto.ca');
-	$(this).attr('href', newHref);
+    $('.email-link').hover(function() {
+        var newHref = $(this).attr('href').replace('unhackable', 'mail.utoronto.ca');
+        $(this).attr('href', newHref);
     });
-    $('#email-link').hover(function(){
-	var newHref = $(this).attr('href').replace('hidemelink', 'me');
-	$(this).attr('href', newHref);
+    $('#email-link').hover(function() {
+        var newHref = $(this).attr('href').replace('hidemelink', 'me');
+        $(this).attr('href', newHref);
     });
     var contactForm = $("#contact-form");
     contactForm.submit(function() {
         event.preventDefault();
         var formDict = {};
-        contactForm.serializeArray().map(function(curr){formDict[curr.name] = curr.value;});
+        contactForm.serializeArray().map(function(curr) {
+            formDict[curr.name] = curr.value;
+        });
         console.log(formDict);
-        if (formDict["g-recaptcha-response"].toString().trim()!==""){
+        if (formDict["g-recaptcha-response"].toString().trim() !== "") {
             $('#captcha-label').hide();
             $.ajax({
                 type: "POST",
                 url: "http://localhost:8081/mailer",
-                data:  JSON.stringify(formDict),
-                success: function(data){
+                data: JSON.stringify(formDict),
+                success: function(data) {
                     console.log(data);
-                    if (data){
+                    if (data) {
                         alertify.notify('Sent', 'success', 2);
                         contactForm.trigger("reset");
-                    }else{
+                    } else {
                         alertify.notify('Sent', 'failed', 2);
                     }
                 },
                 dataType: "json",
-                contentType : "application/json"
+                contentType: "application/json"
             });
 
-        }
-        else {
+        } else {
             $('#captcha-label').show();
         }
-}
+    })
 
 });
