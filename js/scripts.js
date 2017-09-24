8240e9d6c8a5f1e5f22c6326172e47f9562f9799
Kevin Shen <kevin_shen@hotmail.ca>
2017-09-23 21:03:44 -0400

    fixed smooth scrolling and removed unecessary js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,4 @@
 $(document).ready(function () {
-    $('#logo').fadeIn(2000);
-    $('#title').fadeIn(6000);
-    $('.toggle-nav').click(function (e) {
-        $('#menuitems').slideToggle(400);
-        //$('#main').toggleClass("blur .disable");
-        $(this).toggleClass('active');
-        $('.menu ul').toggleClass('active');
-        e.preventDefault();
-    });
     // smooth scrolling
     $("a[href^='#']").on('click', function (e) {
         var hash = this.hash;
