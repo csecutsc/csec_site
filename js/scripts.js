09d9327aa35356f81f9ca412f8ee23e75cc499e3
ViperXYZ <sammy.haque@mail.utoronto.ca>
2018-06-30 15:43:40 -0400

    updated links

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // smooth scrolling
-    $("a[href^='#']").on('click', function(e) {
+    $("a[href^='#']").on('click', function (e) {
         var hash = this.hash;
         $('html, body').animate({
             scrollTop: $(hash).offset().top - 50
-        }, 1000, function() {
+        }, 1000, function () {
             window.location.hash = hash;
         });
         e.preventDefault();
     });
     // makeshift anti-spam function
-    $('.email-link').hover(function() {
+    $('.email-link').hover(function () {
         var newHref = $(this).attr('href').replace('unhackable', 'mail.utoronto.ca');
         $(this).attr('href', newHref);
     });
-    $('.email-link').hover(function() {
+    $('.email-link').hover(function () {
         var newHref = $(this).attr('href').replace('hidemelink', 'me');
         $(this).attr('href', newHref);
     });
-    var contactForm = $("#contact-form");
-    contactForm.submit(function() {
-        event.preventDefault();
-        var formDict = {};
-        contactForm.serializeArray().map(function(curr) {
-            formDict[curr.name] = curr.value;
-        });
-        console.log(formDict);
-        if (formDict["g-recaptcha-response"].toString().trim() !== "") {
-            $('#captcha-label').hide();
-            $.ajax({
-                type: "POST",
-                url: "/mailer",
-                data: JSON.stringify(formDict),
-                success: function(data) {
-                    console.log(data);
-                    if (data["success"]===true) {
-                        alertify.notify('Sent', 'success', 2);
-                        contactForm.trigger("reset");
-                    } else {
-                        alertify.notify('Could not send', 'failed', 2);
-                    }
-                },
-                dataType: "json",
-                contentType: "application/json"
-            });
-
-        } else {
-            $('#captcha-label').show();
-        }
-    })
-
 });
